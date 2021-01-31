766abc8acd14616731ca80a8951f65286d0c6c25
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-01-31 17:49:24 -0300

    Na page de home foi adicionado um breve resumo do sistema

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,45 @@
 import React from "react";
 
+import {
+  Container,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+
 import useStyles from "./styles";
 
 const Home = () => {
   const classes = useStyles();
 
-  return <h1>Home</h1>;
+  return (
+    <Card>
+      <CardContent>
+        <Grid container>
+          <Grid item xs={12} lg={6}>
+            <Typography className={classes.resumeBox} variant="h6" gutterBottom>
+              Um breve resumo do sistema
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              O projeto foi feito para buscar livros da API do Google e mostrar
+              seus detalhes, foi usado a biblioteca <b>React ❤</b>, com as
+              tecnologias, <b>ESLint</b> com padrão standart para organização do
+              código, <b>Babel Root Import </b>
+              para organização de importações, <b>Redux Thunk</b> para controle
+              de estados, <b>Material UI</b> para Kit de UI, <b>Axios</b> para
+              integração com o backend, implementado tratamento de erros no
+              interceptor, <b>Moment</b> para tratamento de datas... entre
+              outras tecnologias.
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Tenha um bom uso :)
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Home;
