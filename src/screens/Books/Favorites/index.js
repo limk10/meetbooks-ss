c49e9dd9780054a2f358bc7bc578b90c8c484c49
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-01-31 17:50:56 -0300

    Page de livros favoritos com dois componentes locais

--- src/screens/Books/Favorites/index.js
+++ src/screens/Books/Favorites/index.js
@@ -1,12 +1,55 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Grid, Typography, Container } from "@material-ui/core";
 
 import useStyles from "./styles";
 
-const Favorites = () => {
-  const classes = useStyles();
+import CardBook from "~/components/CardBook";
 
-  return <h1>Meus Favoritos</h1>;
+import { getBooksLocalStorage } from "~/helpers/localStorage";
+
+const List = () => {
+  const [booksList, setBooksList] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const books = await getBooksLocalStorage();
+    setBooksList([...books]);
+  };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        {!booksList?.length && (
+          <Typography
+            style={{ textAlign: "center" }}
+            variant="overline"
+            display="block"
+            gutterBottom
+          >
+            Nenhum livro encontrado nos seus favoritos :(
+          </Typography>
+        )}
+        {!!booksList?.length && (
+          <>
+            <Typography variant="overline" display="block" gutterBottom>
+              {booksList?.length} Livros Encontrados :)
+            </Typography>
+            <Grid id="list-container" container spacing={2}>
+              {booksList?.map((book, key) => (
+                <Grid key={key} item xs={12} sm={4} md={3}>
+                  <CardBook book={book} />
+                </Grid>
+              ))}
+            </Grid>
+          </>
+        )}
+      </Grid>
+    </Grid>
+  );
 };
 
-export default Favorites;
+export default List;
