ea6a03971554af90bc99aea509564cca96136269
Matheus Lopes <matheus_barbosa5@hotmail.com>
2021-01-31 17:51:48 -0300

    Page de detalhes do livro

--- src/screens/Books/Details/index.js
+++ src/screens/Books/Details/index.js
@@ -1,12 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  LinearProgress,
+} from "@material-ui/core";
+
 import useStyles from "./styles";
+
+import api from "~/services/api";
+
+import { formatBRLMoney } from "~/helpers/money";
+
+import Description from "./components/Description";
+import Characteristics from "./components/Characteristics";
 
 const Details = ({ match }) => {
   const { id } = match.params;
   const classes = useStyles();
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  return <h1>Detalhes do Livro</h1>;
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    try {
+      setLoading(true);
+      const { data } = await api.get(`/${id}`);
+      setBook({ ...data });
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      {loading && (
+        <>
+          <Typography
+            className={classes.loadingText}
+            variant="overline"
+            display="block"
+            gutterBottom
+          >
+            Carregando detalhes do livro, um momento...
+          </Typography>
+          <LinearProgress />
+        </>
+      )}
+      {!loading && (
+        <>
+          <Grid container>
+            <Grid item xs={3}>
+              <img
+                className={classes.media}
+                src={book?.volumeInfo?.imageLinks?.smallThumbnail}
+              />
+            </Grid>
+            <Grid item xs={9}>
+              <Card className={classes.marginLeft}>
+                <CardContent>
+                  <Grid style={{ display: "flex" }} item xs={12}>
+                    <Typography variant="h5" gutterBottom>
+                      {book?.volumeInfo?.title}
+                    </Typography>
+                    <Typography
+                      style={{ flex: 1, textAlign: "right" }}
+                      color="textSecondary"
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      ({book?.id})
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      Autor: {book?.volumeInfo?.authors?.map((item) => item)} |
+                      Editora: {book?.volumeInfo?.publisher}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="subtitle2" gutterBottom>
+                      {book?.volumeInfo?.categories?.length &&
+                        book?.volumeInfo?.categories[0]?.replaceAll("/", "•")}
+                    </Typography>
+                  </Grid>
+                  {book?.saleInfo?.listPrice?.amount && (
+                    <Box mt={3}>
+                      <Grid item xs={12}>
+                        <Typography variant="h4" gutterBottom>
+                          {formatBRLMoney(book?.saleInfo?.listPrice?.amount)}
+                        </Typography>
+                      </Grid>
+                    </Box>
+                  )}
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          <Description book={book} />
+          <Characteristics book={book} />
+        </>
+      )}
+    </div>
+  );
 };
 
 Details.propTypes = {
--- src/screens/Books/Details/styles.js
+++ src/screens/Books/Details/styles.js
@@ -1,5 +1,23 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  media: {
+    width: "85%",
+    height: "auto",
+    borderRadius: "15px",
+  },
+  marginLeft: {
+    boxShadow: "none !important",
+    borderRadius: "15px",
+  },
+  marginTop: {
+    marginTop: theme.spacing(2),
+    boxShadow: "none !important",
+    borderRadius: "15px",
+  },
+  root: {
+    paddingBottom: theme.spacing(6),
+  },
+}));
 
 export default useStyles;
