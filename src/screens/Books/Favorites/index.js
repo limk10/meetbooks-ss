80bc11e2a2c188d6a24f5f709af1996f38004672
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-31 02:03:15 -0300

    Componente base para lista de livros favoritos

---
+++ src/screens/Books/Favorites/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router";
+
+import useStyles from "./styles";
+
+const Favorites = () => {
+  const classes = useStyles();
+
+  return <h1>Meus Favoritos</h1>;
+};
+
+export default Favorites;
